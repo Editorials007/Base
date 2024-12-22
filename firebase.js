6d200e069de12472dc3d4b959c05790e3c1fbf85
Editorials007 <editorials007@gmail.com>
2024-12-22 12:00:39 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC4yrGnytEfpCOf7YPd4LTxdZoz5Ht9UQ4",
+  authDomain: "https://base-b48c7.firebaseapp.com",
+  projectId: "base-b48c7",
+  storageBucket: "https://base-b48c7.firebasestorage.app",
+  messagingSenderId: "280809652608",
+  appId: "1:280809652608:web:e74eda9a6379c7edf9c165"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
